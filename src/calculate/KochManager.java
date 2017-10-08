0ac892fb79fd9a1d17371b6f10fba32056468fed
ImJellyUJelly <jelleschrader@gmail.com>
2017-10-08 15:51:48 +0200

    project

---
+++ src/calculate/KochManager.java
@@ -0,0 +1,35 @@
+package calculate;
+
+import jsf31kochfractalfx.JSF31KochFractalFX;
+
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+public class KochManager implements Observer {
+    private ArrayList<Edge> edges = new ArrayList<Edge>();
+    private int currentLevel;
+    private JSF31KochFractalFX application;
+    private KochFractal koch;
+
+    public KochManager(JSF31KochFractalFX app) {
+        application = app;
+    }
+
+    public void changeLevel(int level) {
+        koch.setLevel(level);
+        drawEdges();
+    }
+
+    public void drawEdges() {
+        application.clearKochPanel();
+        koch.generateLeftEdge();
+        koch.generateBottomEdge();
+        koch.generateRightEdge();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        application.drawEdge((Edge)arg);
+    }
+}
